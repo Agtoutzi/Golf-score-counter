8786f172f3c66383185bf9b8adb6f755418b8749
Agtoutzi <grandthau@hotmail.com>
2017-03-01 08:02:25 +0200

    Implement correct score keeping for all player 1 score buttons

--- app/src/main/java/com/example/android/golfscorecounter/MainActivity.java
+++ app/src/main/java/com/example/android/golfscorecounter/MainActivity.java
@@ -20,6 +20,41 @@ public class MainActivity extends AppCompatActivity {
         displayScore1(score1);
     }
 
+    public void scoredAlbatross(View v){
+        score1-=3;
+        displayScore1(score1);
+    }
+
+    public void scoredEagle(View v){
+        score1-=2;
+        displayScore1(score1);
+    }
+
+    public void scoredBirdie(View v){
+        score1-=1;
+        displayScore1(score1);
+    }
+
+    public void scoredBogey(View v){
+        score1+=1;
+        displayScore1(score1);
+    }
+
+    public void scored2Bogey(View v){
+        score1+=2;
+        displayScore1(score1);
+    }
+
+    public void scored3Bogey(View v){
+        score1+=3;
+        displayScore1(score1);
+    }
+
+    public void failedToScore(View v){
+        score1+=4;
+        displayScore1(score1);
+    }
+
     public void displayScore1(int score){
         TextView scoreView = (TextView) findViewById(R.id.score1);
         scoreView.setText(String.valueOf(score));
