9f540588226b62db618cab784b07cf2a44a9e723
Agtoutzi <grandthau@hotmail.com>
2017-03-01 07:25:19 +0200

    Add function for condor button click

--- app/src/main/java/com/example/android/golfscorecounter/MainActivity.java
+++ app/src/main/java/com/example/android/golfscorecounter/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.android.golfscorecounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    int score1=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void scoredCondor(View v){
+        score1-=4;
+        displayScore1(score1);
+    }
+
+    public void displayScore1(int score){
+
     }
 }
